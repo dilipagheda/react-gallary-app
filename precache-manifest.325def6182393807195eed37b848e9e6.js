3ee90bdba4d09eae87e8659442e06c05b05c0df0
Dilip Agheda <dilip_agheda@yahoo.com>
2020-03-01 18:50:57 +1100

    Updates

---
+++ precache-manifest.325def6182393807195eed37b848e9e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f5588513de2829d453f",
+    "url": "/react-gallary-app/static/css/main.35ac0c32.chunk.css"
+  },
+  {
+    "revision": "8f5588513de2829d453f",
+    "url": "/react-gallary-app/static/js/main.8f558851.chunk.js"
+  },
+  {
+    "revision": "40f8fd9f0cfc14eb4bd8",
+    "url": "/react-gallary-app/static/js/1.40f8fd9f.chunk.js"
+  },
+  {
+    "revision": "deab69e0ade6e7d6b2ec",
+    "url": "/react-gallary-app/static/js/runtime~main.deab69e0.js"
+  },
+  {
+    "revision": "3970d8604b5b0399a68c4f572e5326d3",
+    "url": "/react-gallary-app/index.html"
+  }
+];
